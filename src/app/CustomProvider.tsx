73c740b47d5005f4839d4ba78314ce2850472fec
Juiiceee <lbehr@student.42.fr>
2024-12-23 17:43:42 +0100

    first commit

---
+++ src/app/CustomProvider.tsx
@@ -0,0 +1,41 @@
+"use client";
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+	getDefaultConfig,
+	RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import {
+	mainnet,
+	arbitrum,
+	sepolia,
+	anvil
+} from 'wagmi/chains';
+import {
+	QueryClientProvider,
+	QueryClient,
+} from "@tanstack/react-query";
+import React from 'react';
+
+const config = getDefaultConfig({
+	appName: 'My RainbowKit App',
+	projectId: '5aa666787d59303d9eed8e3401d2237d',
+	chains: [mainnet, arbitrum, sepolia, anvil],
+	ssr: true,
+});
+
+const queryClient = new QueryClient();
+const CustomProvider = ({ children, }: Readonly<{
+	children: React.ReactNode;
+}>) => {
+	return (
+		<WagmiProvider config={config}>
+			<QueryClientProvider client={queryClient}>
+				<RainbowKitProvider>
+					{children}
+				</RainbowKitProvider>
+			</QueryClientProvider>
+		</WagmiProvider>
+	);
+};
+export default CustomProvider;
---
+++ src/app/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { useAccount, useDisconnect } from "wagmi";
+import { useState } from "react";
+import { Button, Card } from "antd";
+
+export default function Home() {
+	const { address } = useAccount();
+	const [pubkey, setPubkey] = useState("");
+	const [name, setName] = useState("");
+	const [description, setDescription] = useState("");
+
+	const sendFirst =() => {
+		console.log("Pubkey: ", pubkey);
+	}
+
+	const sendSecond =() => {
+		console.log("Name: ", name);
+		console.log("Description: ", description);
+	}
+
+	return (
+		<>
+			<div className="flex justify-center items-center flex-col">
+				<Card title="Chercher un vote" style={{ width: 300, marginBottom: 20 }}>
+					<input type="text" id="pubkey" placeholder="Pubkey du contrat" className="pl-2 mb-5" onChange={(e) => {setPubkey(e.target.value)}}/>
+					<Button disabled={!pubkey} onClick={sendFirst}>Chercher</Button>
+				</Card>
+
+				<Card title="Creer un vote" style={{ width: 300}}>
+					<input type="text" id="name" placeholder="Nom du vote" className="pl-2 mb-5 " onChange={(e) => {setName(e.target.value)}}/>
+					<input type="text" id="description" placeholder="Description du vote" className="pl-2 mb-5" onChange={(e) => {setDescription(e.target.value)}}/>
+					<Button disabled={!name || !description} onClick={sendSecond}>Creer un vote</Button>
+				</Card>
+			</div>
+		</>
+	);
+}
